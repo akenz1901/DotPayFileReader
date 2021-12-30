dfd78f26cce90af1bf112cca8f8373f99de731e0
OkoroKelvin <okorokelvinemoakpooke@yahoo.com>
2021-12-30 07:24:13 +0100

    Project Setup

---
+++ src/main/java/com/kelvin/onepipechallenge/OnePipeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.kelvin.onepipechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnePipeChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnePipeChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kelvin/onepipechallenge/OnePipeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kelvin.onepipechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePipeChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
